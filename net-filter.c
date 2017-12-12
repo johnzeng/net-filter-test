72bf23dcf9d956fc09653149c4c7f80e4260021a
johnzeng <zengda@yunba.io>
2017-12-12 13:24:45 +0800

    net-filter counter is done

--- net-filter.c
+++ net-filter.c
@@ -1,15 +1,44 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/netfilter.h>
+
+unsigned int counter = 0;
+unsigned int nf_hookfn_package_counter(
+        unsigned int hooknum,
+        struct sk_buffer *skb,
+        const struct net_device *in,
+        const struct net_device *out,
+        int(*okfn)(struct sk_buff*))
+{
+    counter ++;
+    if(counter > 100)
+    {
+        printk("100 counter reach, reset\n");
+        counter = 0;
+    }
+    return NF_ACCEPT;
+}
+
+static struct nf_hook_ops _hook_ops = {
+    .hook = nf_hookfn_package_counter,
+    .owner = THIS_MODULE,
+    .pf = NFPROTO_IPV4,
+    .hooknum = NF_INET_PRE_ROUTING,
+    .priority = 0,
+};
 
 static int net_filter_test_init(void)
 {
-        printk(KERN_INFO "[init] Can you feel me?\n");
-            return 0;
+    printk(KERN_INFO "[init] now init the netfilter hook\n");
+    int reg_result = nf_register_hook(&_hook_ops);
+    printk(KERN_INFO "reg_result:%d\n", reg_result);
+    return 0;
 }
 
 static void net_filter_test_exit(void)
 {
-        printk(KERN_INFO "[exit] Yes.\n");
+    nf_unregister_hook(&_hook_ops);
+    printk(KERN_INFO "[exit] unregister done:\n");
 }
 
 
